61039ff69e9a2b2caefb3b29b247acdb8e6a17ca
Nguyễn Nhật Mai <111487792+mion158@users.noreply.github.com>
2022-09-04 15:28:26 +0100

    added a file

---
+++ Generative chatbot/seq2seq.py
@@ -0,0 +1,25 @@
+from prep import input_features_dict, target_features_dict, reverse_input_features_dict, reverse_target_features_dict, max_decoder_seq_length, input_docs, target_docs, input_tokens, target_tokens, max_encoder_seq_length
+from training_model import decoder_inputs, decoder_lstm, decoder_dense, encoder_input_data, num_decoder_tokens, num_encoder_tokens
+
+from tensorflow import keras
+from keras.layers import Input, LSTM, Dense
+from keras.models import Model, load_model
+import numpy as np
+import re
+
+training_model = load_model('training_model.h5')
+#because we're working with a saved model
+encoder_inputs = training_model.input[0]
+encoder_outputs, state_h_enc, state_c_enc = training_model.layers[2].output
+encoder_states = [state_h_enc, state_c_enc]
+
+encoder_model = Model(encoder_inputs, encoder_states)
+
+latent_dim = 256
+decoder_state_input_hidden = Input(shape=(latent_dim,))
+decoder_state_input_cell = Input(shape=(latent_dim,))
+decoder_states_inputs = [decoder_state_input_hidden, decoder_state_input_cell]
+decoder_outputs, state_hidden, state_cell = decoder_lstm(decoder_inputs, initial_state=decoder_states_inputs)
+decoder_states = [state_hidden, state_cell]
+decoder_outputs = decoder_dense(decoder_outputs)
+decoder_model = Model([decoder_inputs] + decoder_states_inputs, [decoder_outputs] + decoder_states)
